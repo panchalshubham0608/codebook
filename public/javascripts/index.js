2cddb24f9ec9d8e0aac23d9ef8773b140b98d010
Shubham Panchal <shubhampanchal9773@gmail.com>
2023-06-05 11:38:09 +0530

    add browse via link

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,69 @@
+// process a problem
+function processProblem(content) {
+    // try to set the problem object at /api/problem
+    // display loader
+    document.getElementById('loading-wrapper').style.display = 'block';
+    // send request to /api/problem
+    try {
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', '/api/problem');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        // handle response
+        xhr.onload = () => {
+            if (xhr.status == 200) {
+                // redirect to /api/problem
+                window.location.href = '/api/problem';
+            } else if (xhr.status == 400) {
+                alert(`Error: ${JSON.parse(xhr.responseText).error}`);
+            } else {
+                alert('Something went wrong');
+            }
+        }
+        // handle error
+        xhr.onerror = () => {
+            if (xhr.status == 400) {
+                alert(JSON.parse(xhr.responseText).error);
+            } else {
+                alert('Something went wrong');
+            }
+        }
+        // send the request with the problem object
+        xhr.send(JSON.stringify({ problem: content }));
+    } catch (err) {
+        console.log(err);
+        alert('Something went wrong');
+    } finally {
+        // hide loader
+        document.getElementById('loading-wrapper').style.display = 'none';
+        // reload page
+        window.location.reload();
+    }
+}
+
+// fetch a problem via link
+function browseViaLink() {
+    // get link to problem
+    let link = prompt('Enter link to problem', '');
+    // check if link is empty
+    if (link == null || link == '') return;
+    // display loader
+    document.getElementById('loading-wrapper').style.display = 'block';
+    // fetch contents of the link
+    fetch(link).then((response) => {
+        return response.text();
+    }).then((data) => {
+        // process the problem
+        return processProblem(data);
+    }).catch((err) => {
+        console.log(err);
+        alert('Something went wrong');
+    }).finally(() => {
+        // hide loader
+        document.getElementById('loading-wrapper').style.display = 'none';
+    });
+}
+
+
 // add onchange handler to upload-button
 document.addEventListener('DOMContentLoaded', () => {
     // get upload button
@@ -50,43 +116,8 @@ document.addEventListener('DOMContentLoaded', () => {
                     return;
                 }
             }
-            // try to set the problem object at /api/problem
-            // display loader
-            document.getElementById('loading-wrapper').style.display = 'block';
-            // send request to /api/problem
-            try {
-                let xhr = new XMLHttpRequest();
-                xhr.open('POST', '/api/problem');
-                xhr.setRequestHeader('Content-Type', 'application/json');
-                // handle response
-                xhr.onload = () => {
-                    if (xhr.status == 200) {
-                        // redirect to /api/problem
-                        window.location.href = '/api/problem';
-                    } else if (xhr.status == 400) {
-                        alert(`Error: ${JSON.parse(xhr.responseText).error}`);
-                    } else {
-                        alert('Something went wrong');
-                    }
-                }
-                // handle error
-                xhr.onerror = () => {
-                    if (xhr.status == 400) {
-                        alert(JSON.parse(xhr.responseText).error);
-                    } else {
-                        alert('Something went wrong');
-                    }
-                }
-                // send the request with the problem object
-                xhr.send(JSON.stringify({ problem: content }));
-            } catch (err) {
-                console.log(err);
-                alert('Something went wrong');
-            } finally {
-                // hide loader
-                document.getElementById('loading-wrapper').style.display = 'none';
-            }
-
+            // process the problem
+            processProblem(content);
         }
         // handle error while reading file
         reader.onerror = () => {
