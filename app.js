a8a9e11c66e05af65df7354e56d8509035754406
Shubham Panchal <shubhampanchal9773@gmail.com>
2023-06-11 10:08:08 +0530

    fix payload too large

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
